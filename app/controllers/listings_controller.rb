9b1adc7333d05cb5c006ce416fa2d5a3995b78bc
Nathan Tu <nathan@globalhunch.com>
2019-04-17 14:55:27 +1000

    added controller and methods plus views for listings

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,28 @@
+class ListingsController < ApplicationController
+    
+    def index
+        # show all the listings
+    end
+
+    def create
+        # 
+    end
+
+    def new
+        # shows form for creating the listings
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+
+
+end
