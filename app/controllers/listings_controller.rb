f63a7d87d7a6370e0f6098d0828970b9f2f729c5
Nathan Tu <nathan@globalhunch.com>
2019-04-17 15:25:44 +1000

    Added in method to Controller and before-action

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,13 @@
 class ListingsController < ApplicationController
+    before_action :set_listing, only: [:show, :edit, :update, :destroy]
     
     def index
         # show all the listings
+        @listings = Listing.all
     end
 
     def create
-        # 
+        # create new listing
     end
 
     def new
@@ -13,16 +15,30 @@ class ListingsController < ApplicationController
     end
 
     def show
+        # view a single listing
+        
     end
 
     def update
+        # update the current listing
+        
     end
 
     def edit
+        # show the edit form
+        
     end
 
     def destroy
+        # deletes current listing
+        
     end
+
+    def set_listing
+        id = params[:id]
+        @listing = Lising.find(id)
+    end
+
 
 
 end
