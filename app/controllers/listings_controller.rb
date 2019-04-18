777bf6f5fcdd0f65211bd7baf45dd234b4bbb3dc
Nathan Tu <nathan@globalhunch.com>
2019-04-18 13:03:32 +1000

    Created form partial and used in new and edit views

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,7 @@
 class ListingsController < ApplicationController
     before_action :set_listing, only: [:show, :edit, :update, :destroy]
-    
+    before_action :set_breeds_and_sexes, only: [:new, :edit]
+   
     def index
         # show all the listings
         @listings = Listing.all
@@ -10,16 +11,21 @@ class ListingsController < ApplicationController
         # create new listing
        @listing = Listing.create(listing_params)
         # byebug #this byebug is for debugging
-        byebug
+        # byebug
+        if @listing.errors.any?
+            set_breeds_and_sexes
+            render "new"
+        else
+            redirect_to listings_path
+        end
     end
-
-    
 
     def new
         # shows form for creating the listings
         @listing = Listing.new
-        @breeds = Breed.all
-        @sexes = Listing.sexes.keys
+        
+
+
     end
 
     def show
@@ -43,9 +49,14 @@ class ListingsController < ApplicationController
     end
 
     private
+    def set_breeds_and_sexes
+        @breeds = Breed.all
+        @sexes = Listing.sexes.keys
+    end
+    
     def set_listing
         id = params[:id]
-        @listing = Lising.find(id)
+        @listing = Listing.find(id)
     end
 
     def listing_params
